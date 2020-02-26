177c4936dfacb258b555692f5e2efe27012ccacc
link <uixlin@163.com>
2020-02-26 10:05:01 +0800

    添加重采样

--- test_resmple.c
+++ test_resmple.c
@@ -4,5 +4,8 @@
 int main(){
     SpeexResamplerState *resampler;
     int err = 0;
-    resampler = speex_resampler_init(1,48000,16000,10,&err)
+    resampler = speex_resampler_init(1,48000,16000,10,&err);
+    FILE *f;
+    f = fopen("/mnt/d/workSpacess/test.pcm", "rb+");
+    return 0;
 }
